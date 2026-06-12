74c92fded9f569290140934ec439ff662546a630
abhi-saurav-saroya <abhisauravsaroya06@gmail.com>
2026-06-12 22:47:27 +0530

    all texts

---
+++ messages.py
@@ -0,0 +1,47 @@
+WELCOME_MESSAGE = """
+          ================================================
+                            TWINSCAN
+                Detect duplicates. Reclaim space.
+                   Powered by SHA-256 Hashing
+          ================================================
+"""
+
+ABOUT_TWINSCAN = """
+                            TwinScan
+          A Python-based duplicate file finder that scans
+            directories, detects identical files using
+             hashing, and helps reclaim storage space.
+
+                         Created by Abhi
+       (LinkedIn: https://www.linkedin.com/in/abhi-saurav-saroya/)
+"""
+
+MAIN_MENU = """
+MAIN MENU:
+    1. Scan Directory
+    2. View Last Scan Results
+    3. About
+    4. Exit
+"""
+
+GOODBYE_MESSAGE = """
+          ================================================
+                  Thank you for using TwinScan!
+                     Scan smart. Save space.
+          ================================================
+"""
+
+SUCCESS_MESSAGE = """
+          ================================================
+              TwinScan completed the scan successfully.
+                 View scan's results from main menu.
+          ================================================
+"""
+
+ERROR_MESSAGE = """
+          ================================================
+                       An error occurred.
+                 TwinScan was unable to complete
+                       the requested scan.
+          ================================================
+"""
